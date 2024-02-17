6bd0894ece93b3172c40ce3db58ae3fa9fccd09d
Dev-with-Osm <khourssaoussama7@gmail.com>
2024-02-17 19:50:11 +0100

    complete image upload func

--- client/src/Pages/Profile.jsx
+++ client/src/Pages/Profile.jsx
@@ -1,21 +1,92 @@
+import { useEffect, useRef, useState } from "react";
 import { CiAt } from "react-icons/ci";
 import { IoLockClosedOutline } from "react-icons/io5";
 import { MdOutlineContactPage, MdOutlineEdit } from "react-icons/md";
 import { useSelector } from "react-redux";
+import {
+  getDownloadURL,
+  getStorage,
+  ref,
+  uploadBytesResumable,
+} from "firebase/storage";
+import { app } from "../firebase";
 
 export default function Profile() {
+  const fileRef = useRef(null);
   const { currentUser } = useSelector((state) => state.user);
+  const [file, setFile] = useState(undefined);
+  const [filePerc, setFilePerc] = useState(0);
+  const [fileUploadError, setFileUploadError] = useState(false);
+
+  const [formData, setFormData] = useState({});
+
+  console.log(formData);
+  console.log(fileUploadError);
+  console.log(filePerc);
+
+  useEffect(() => {
+    if (file) {
+      handleFileUpload(file);
+    }
+  }, [file]);
+
+  const handleFileUpload = (file) => {
+    const storage = getStorage(app);
+    const fileName = new Date().getTime() + file.name;
+    const storageRef = ref(storage, fileName);
+    const uploadTask = uploadBytesResumable(storageRef, file);
+
+    uploadTask.on(
+      "state_changed",
+      (snapShot) => {
+        const progress =
+          (snapShot.bytesTransferred / snapShot.totalBytes) * 100;
+        setFilePerc(Math.round(progress));
+      },
+      (error) => {
+        setFileUploadError(error.message);
+      },
+      () => {
+        getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+          setFormData({ ...formData, avatar: downloadURL });
+        });
+      }
+    );
+  };
   return (
     <div className="flex flex-col my-10 justify-center items-center p-2">
       <h1 className="flex items-center justify-center text-2xl gap-2 mb-4 font-medium">
         Edit your profile
         <MdOutlineEdit />
       </h1>
-      <img
-        src={currentUser.avatar}
-        alt="avatar"
-        className="w-24 h-24 object-cover cursor-pointer mb-4 rounded-full"
+      <input
+        onChange={(e) => setFile(e.target.files[0])}
+        type="file"
+        ref={fileRef}
+        hidden
+        accept="image/*"
       />
+
+      <img
+        onClick={() => fileRef.current.click()}
+        src={formData.avatar || currentUser.avatar}
+        alt="avatar"
+        className="w-24 h-24 object-cover cursor-pointer mb-4 rounded-full "
+      />
+      <p className="text-sm self-center mb-3">
+        {fileUploadError ? (
+          <span className="text-red-700">
+            Error Image upload (image must be less than 2 mb)
+          </span>
+        ) : filePerc > 0 && filePerc < 100 ? (
+          <span className="text-slate-700">{`Uploading ${filePerc}%`}</span>
+        ) : filePerc === 100 ? (
+          <span className="text-green-700">Image successfully uploaded!</span>
+        ) : (
+          ""
+        )}
+      </p>
+
       <form className="flex flex-col gap-5 bg-transparent border-2 border-black p-8 md:w-[450px] rounded-lg">
         <div className="flex flex-col gap-4">
           <label className="text-black font-semibold">Username</label>
